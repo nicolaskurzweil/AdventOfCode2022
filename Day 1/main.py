5b5c39edbbaae508c735cfc6c0f0fe8b51d8a7eb
Nicolas Kurzweil <nicolas_kurzweil@web.de>
2023-02-01 22:22:28 +0100

    optimize

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,21 +1,11 @@
 # Read Content of input file
-f = open("Day 1/input", "r")
-rows = f.readlines()
-f.close()
-
-elves = [[]]
-
-for row in rows:
-    if not row.strip().isdigit():
-        elves.append([])
-    else:
-        elves[len(elves) - 1].append(int(row.strip()))
+with open("Day 1/input", "r") as f:
+  rows = f.read()
+elves = rows.split("\n\n")
 
 # Calculate sum of calories
-index = 0
 for elf in elves:
-    elves[index] = sum(elf)
-    index += 1
+    elves[elves.index(elf)] = sum(list(map(int, elf.split("\n"))))
 
 # Get max values
 elves.sort(reverse=True)
