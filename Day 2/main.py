454f213b200a21840c5b287b7a08b07de5e62ba8
Nicolas Kurzweil <nicolas_kurzweil@web.de>
2023-02-03 19:31:50 +0100

    optimize day 2

--- Day 2/main.py
+++ Day 2/main.py
@@ -4,17 +4,12 @@ total_score = 0
 
 # Read Content of input file
 with open("Day 2/input", "r") as f:
-    rows = f.read().replace("A", "Rock").replace("B", "Paper").replace("C", "Scissors").replace("X", "Rock").replace("Y", "Paper").replace("Z", "Scissors").split("\n")
+    rows = f.read().strip("\n").replace("A", "Rock").replace("B", "Paper").replace("C", "Scissors").replace("X", "Rock").replace("Y", "Paper").replace("Z", "Scissors").split("\n")
 
 # Calculate score
 for row in rows:
-    if len(row.split(" ")) == 2:
-        if row == "Scissors Rock" or row == "Rock Paper" or row == "Paper Scissors":
-            total_score += points_result["Won"]
-        elif row == "Rock Scissors" or row == "Paper Rock" or row == "Scissors Paper":
-            total_score += points_result["Lost"]
-        elif row == "Rock Rock" or row == "Paper Paper" or row == "Scissors Scissors":
-            total_score += points_result["Draw"]
-        total_score += points_chosen[row.split(" ")[1]] 
+    total_score += points_result["Won"] + points_chosen[row.split(" ")[1]] if row == "Rock Paper" or row == "Paper Scissors" or row == "Scissors Rock" else 0
+    total_score += points_result["Lost"] + points_chosen[row.split(" ")[1]] if row == "Rock Scissors" or row == "Paper Rock" or row == "Scissors Paper" else 0
+    total_score += points_result["Draw"] + points_chosen[row.split(" ")[1]] if row == "Rock Rock" or row == "Paper Paper" or row == "Scissors Scissors" else 0
 
 print(total_score)
--- Day 2/main_2.py
+++ Day 2/main_2.py
@@ -4,23 +4,15 @@ total_score = 0
 
 # Read Content of input file
 with open("Day 2/input", "r") as f:
-    rows = f.readlines()
+    rows = f.read().strip("\n").replace("A", "Rock").replace("B", "Paper").replace("C", "Scissors").replace("X", "Lose").replace("Y", "Draw").replace("Z", "Win")
 
-# Find out what I need to play
-content = []    
-for row in rows:
-    temp = row.replace("A", "Rock").replace("B", "Paper").replace("C", "Scissors").replace("X", "Lose").replace("Y", "Draw").replace("Z", "Win").strip()
-    temp = temp.replace("Rock Lose", "Rock Scissors").replace("Paper Lose", "Paper Rock").replace("Scissors Lose", "Scissors Paper").replace("Rock Draw", "Rock Rock").replace("Paper Draw", "Paper Paper").replace("Scissors Draw", "Scissors Scissors").replace("Rock Win", "Rock Paper").replace("Paper Win", "Paper Scissors").replace("Scissors Win", "Scissors Rock")
-    content.append(temp)                  
+# Find out what I need to play                
+content = rows.replace("Rock Lose", "Rock Scissors").replace("Paper Lose", "Paper Rock").replace("Scissors Lose", "Scissors Paper").replace("Rock Draw", "Rock Rock").replace("Paper Draw", "Paper Paper").replace("Scissors Draw", "Scissors Scissors").replace("Rock Win", "Rock Paper").replace("Paper Win", "Paper Scissors").replace("Scissors Win", "Scissors Rock").split("\n")
 
 # Calculate score
 for row in content:
-    if row == "Scissors Rock" or row == "Rock Paper" or row == "Paper Scissors":
-        total_score += points_result["Won"]
-    elif row == "Rock Scissors" or row == "Paper Rock" or row == "Scissors Paper":
-        total_score += points_result["Lost"]
-    elif row == "Rock Rock" or row == "Paper Paper" or row == "Scissors Scissors":
-        total_score += points_result["Draw"]
-    total_score += points_chosen[row.split(" ")[1]]            
+    total_score += points_result["Won"] + points_chosen[row.split(" ")[1]] if row == "Rock Paper" or row == "Paper Scissors" or row == "Scissors Rock" else 0
+    total_score += points_result["Lost"] + points_chosen[row.split(" ")[1]] if row == "Rock Scissors" or row == "Paper Rock" or row == "Scissors Paper" else 0
+    total_score += points_result["Draw"] + points_chosen[row.split(" ")[1]] if row == "Rock Rock" or row == "Paper Paper" or row == "Scissors Scissors" else 0
 
 print(total_score)
