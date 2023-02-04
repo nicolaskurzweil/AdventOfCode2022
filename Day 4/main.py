92ffae664a3a81e04cae4080bbd65130dd0d62db
Nicolas Kurzweil <nicolas_kurzweil@web.de>
2023-02-04 12:01:24 +0100

    Day 4

---
+++ Day 4/main.py
@@ -0,0 +1,14 @@
+# Read input file
+with open("Day 4/input", "r") as f:
+    rows = f.read().strip("\n").split("\n")
+
+counter = 0    
+
+for row in rows:
+    elves = row.split(",")
+    elf_one = list(map(int, elves[0].split("-")))
+    elf_two = list(map(int, elves[1].split("-")))
+    if (elf_one[0] >= elf_two[0] and elf_one[1] <= elf_two[1]) or (elf_two[0] >= elf_one[0] and elf_two[1] <= elf_one[1]):
+        counter += 1
+
+print(counter)
