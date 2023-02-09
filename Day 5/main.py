ad5ec057e5b054d7cc8d6fbbf2c9fb45cd50847c
Nicolas Kurzweil <nicolas_kurzweil@web.de>
2023-02-09 16:46:45 +0100

    Day 5

---
+++ Day 5/main.py
@@ -0,0 +1,35 @@
+# Read input file
+with open("Day 5/input.txt", "r") as f:
+    rows = f.read().strip("\n").split("\n\n")
+
+temp = rows[0].strip("\n")
+rules = rows[1].strip("\n").strip(" ").split("\n")
+
+# Create stacks
+stacks: dict[int, list] = {}
+for stack in range(1, int(temp.strip(" ")[-1])+1):
+    stacks[stack] = []
+
+temp = temp.split("\n")
+temp.pop()
+
+for stack in stacks.items():
+    move_to_right = (int(stack[0]) - 1) * 4
+    for row in temp:
+        if row[move_to_right:move_to_right+3] != "   ":
+            stacks[stack[0]].append(row[move_to_right:move_to_right+3].replace("[", "").replace("]", ""))
+
+# Apply rules
+for rule in rules:
+    amount = int(rule.split(" ")[1])
+    origin = int(rule.split(" ")[3])
+    destination = int(rule.split(" ")[5])
+
+    for i in range(1, amount+1):
+        stacks[destination].insert(0, stacks[origin].pop(0))
+
+# Print result
+result = ""
+for stack in stacks.values():
+    result = result + stack[0]
+print(result)
