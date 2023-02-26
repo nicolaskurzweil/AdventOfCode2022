3b3875be66fabac8733f1f8f1c93a3f05883a16b
Nicolas Kurzweil <nicolas_kurzweil@web.de>
2023-02-26 11:19:42 +0100

    Day 6

---
+++ Day 6/main.py
@@ -0,0 +1,11 @@
+# Read input file
+with open("Day 6/input", "r") as f:
+    input = f.read().strip("\n")
+
+possible_marker = ""
+
+for char in input:
+    possible_marker = possible_marker.split(char)[1] + char if char in possible_marker else possible_marker + char
+    if len(possible_marker) == 4: break
+        
+print(input.index(possible_marker) + 4)
